81d587a286f26db4d18c4afc352c105df0f6c7f0
Coedgy <fugi501@gmail.com>
2021-12-05 21:54:31 +0200

    Rename MainWindow to LoginWindow, create MainScreen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CSharp_Poker
 {
     /// <summary>
-    /// Interaction logic for MainScreen.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainScreen : Window
+    public partial class MainWindow : Window
     {
-        public MainScreen()
+        public MainWindow()
         {
             InitializeComponent();
         }
