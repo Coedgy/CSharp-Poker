2aafe943cb7e0c46c32025f6325400155faaadf1
Coedgy <fugi501@gmail.com>
2021-12-07 21:32:54 +0200

    finish login window prototype

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace CSharp_Poker
 {
@@ -24,6 +26,7 @@ namespace CSharp_Poker
     public partial class LoginWindow : Window
     {
         private bool loginAction = false;
+        private bool loginCheckRunning = false;
 
         static HttpClient client = new HttpClient();
 
@@ -47,7 +50,7 @@ namespace CSharp_Poker
             {
                 List<KeyValuePair<string, string>> formContent = new List<KeyValuePair<string, string>>();
                 formContent.Add(new KeyValuePair<string, string>("username", UsernameInput.Text));
-                formContent.Add(new KeyValuePair<string, string>("password", PasswordInput.Text));
+                formContent.Add(new KeyValuePair<string, string>("password", PasswordInput.Password));
 
                 var content = new FormUrlEncodedContent(formContent);
 
@@ -72,13 +75,59 @@ namespace CSharp_Poker
 
         private async void SubmitButtonClick(object sender, RoutedEventArgs e)
         {
-            if (await LoginAction())
+            if (!loginCheckRunning)
             {
-                loginAction = true;
-                this.Close();
+                loginCheckRunning = true;
+                if (await LoginAction())
+                {
+                    loginAction = true;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("lol"); //TODO: Proper error messages
+                }
+                
+                loginCheckRunning = false;
             }
+        }
 
-            MessageBox.Show("lol"); //TODO: Proper error messages
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            OpenUrl("https://niisto.fi");
+        }
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
